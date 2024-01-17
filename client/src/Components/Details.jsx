5e1d2a6745715186c5f518aeae3e687bd85c5202
Shashant Kashyap <shashant.kashyap999@gmail.com>
2024-01-17 19:00:52 +0530

    Update Details.jsx

--- client/src/Components/Details.jsx
+++ client/src/Components/Details.jsx
@@ -9,7 +9,7 @@ function Details() {
 
   const handleSubmit = async () => {
     try {
-      const response = await axios.post('https://server-ki5g.onrender.com/api/fetch', { hID });
+      const response = await axios.post('https://back-aply.onrender.com/api/fetch', { hID });
       setData(response.data.details);
       console.log(response);
     } catch (error) {
